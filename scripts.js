d36f40e48043f4255e7218d713a68a88efbfa161
Hala <Hala.moa.Idris@gmail.com>
2022-12-01 20:50:15 -0500

    HTML: working. CSS: done. JS: main functionality and logic is working, there is room for additions such as AI , highlight winner etc.

--- scripts.js
+++ scripts.js
@@ -38,32 +38,136 @@ const gameBoard = (()=>{ // module pattern
   const boardArr = ['','','','','','','','',''];
   const setCell = (index, sign) => {
     if (index > boardArr.length) return;
-    board[index] = sign;
+    boardArr[index] = sign;
   };
 
   const getCell = (index) => {
     if (index > boardArr.length) return;
-    return board[index];
+    return boardArr[index];
   };
 
-  const resetBoard = () => {
+  const reset = () => {
     for(let i in boardArr){
       boardArr[i]="";}
   }
 
-  return {setCell, getCell, resetBoard}
+  return {setCell, getCell, reset}
 })();
 
+const Player = (sign)=>{ //factory
+  sign;
+  
+  const getSign = () =>{
+    return sign;
+  }
 
-const players = { //factory
+  return {getSign};
 
 }
 
-const gameControl = (() => { //module
 
+const displayControl = (() => { //module pattern
 
+  const status = document.querySelector('.status');
+  const cells = document.querySelectorAll('.cell');
+  const restartBtn = document.getElementById('restart-btn');
 
+  cells.forEach((cell)=>
+    cell.addEventListener('click', (e) => {
+      if(gameControl.getIsGameOver() || e.target.innerText !=="") return;
+      gameControl.playRound(parseInt(e.target.dataset.index));
+      updateGame();
 
+    })
+  );
 
+  restartBtn.addEventListener('click', (e) =>{
+    gameBoard.reset();
+    gameControl.reset();
+    updateGame();
+    setStatus(false,'X');
+  });
+
+  const updateGame = () =>{
+    for ( let i in cells){
+      cells[i].textContent = gameBoard.getCell(i);
+    }
+  };
+
+  const setStatus = (isOver,winner)=>{
+    if(isOver){
+      if ( winner === "draw") {
+        message = "It's a draw!";
+      }else {
+        message = `Player ${winner} won!`;
+      }
+   }else {
+    message = `Player ${winner} turn`;}
+
+    status.textContent = message;
+  } // modify for draw condition
+ 
+
+  return{updateGame, setStatus};
+
+})();
+
+const gameControl = (()=>{ //Module Pattern
+  
+  const playerX = Player('X');
+  const playerO = Player('O');
+  var round = 1;
+  var gameOver =false;
+
+    const playRound = (idx)=>{
+      gameBoard.setCell(idx,getPlayerSign());
+      if (checkWinner(idx)) {
+        gameOver=true;
+        displayControl.setStatus(gameOver,getPlayerSign());
+        return;
+      }
+      if (round >8){
+        gameOver = true;
+        displayControl.setStatus(gameOver,"draw");
+        return;
+      }
+      round++;
+      displayControl.setStatus(gameOver,getPlayerSign());
+      
+
+    }
+
+    const getPlayerSign = ()=>{
+      let sign = round % 2; /* === 1 ? playerX.getSign() : playerO.getSign(); */
+      if(sign ===1){
+        return playerX.getSign();
+      }else {
+        return playerO.getSign();
+      }
+    }
+
+    const checkWinner = (idx)=>{
+        const winCombinations = [
+          [0,1,2],[3,4,5],[6,7,8],
+          [0,3,6],[1,4,7],[2,5,8],
+          [0,4,8],[2,4,6]
+        ];
+        
+        return winCombinations
+        .filter((combination) => combination.includes(idx))
+        .some((possibleCombination) => possibleCombination.every(
+            (idx) => gameBoard.getCell(idx) === getPlayerSign()
+          )
+        );
+    }
+    const reset = () => {
+      round = 1;
+      gameOver = false;
+    };
+    const getIsGameOver = ()=>{
+      return gameOver;
+    }
+
+    return {playRound,reset,getIsGameOver};
 })();
 
